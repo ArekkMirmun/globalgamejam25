bd70532d145517c06f7fb54aad2e91699a23025f
VandurTheWizard <TheWizardVandur@gmail.com>
2025-01-25 00:08:02 +0100

    Sistema de vida terminado
    Se ha creado el sistema de vida, el cual puede o no depender de una segunda imagen para mostrar el hueco de vida dañado, como esto depende del diseño que se haga al final ya estaría creada para ambos casos

--- Assets/Scripts/Ivan/LifeSystem.cs
+++ Assets/Scripts/Ivan/LifeSystem.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,37 +6,78 @@ public class LifeSystem : MonoBehaviour
 {
 
     [Header("Configuracion")]
-    [SerializeField] private int maxLifes = 3;
-    [SerializeField] private float separation = 10f;
+    [SerializeField] private int maxLifes = 3; // Número de vidas
+    [SerializeField] private float separation = 10f; // Separación entre las vidas
+    [SerializeField] private float size = 50f; // Tamaño de la vida
 
     [Header("Referencias")]
-    [SerializeField] private GameObject parentBackground;
-    [SerializeField] private GameObject parentLife;
-    [SerializeField] private GameObject lifeImage;
+    [SerializeField] private GameObject parentLife; // Padre de las vidas
+    [SerializeField] private Sprite lifeImage; // Imagen de la vida
+    [SerializeField] private Sprite emptyLifeImage; // Puede estar vacio o no
 
-    private GameObject[] background;
-    private GameObject[] lifes;
+    private GameObject[] lifes; // Para facilitar la vida en la gestión de la vida
 
-    private int currentLife;
+    private int currentLife; // Vida actual
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentLife = maxLifes;
-        background = new GameObject[maxLifes];
         lifes = new GameObject[maxLifes];
 
         for (int i = 0; i < maxLifes; i++)
         {
-            background[i] = Instantiate(parentBackground, parentLife.transform);
-            // Se pone como padre el parenBackground para que se vean en el mismo lugar
-            
-            background[i].transform.localPosition = new Vector3(i * separation, 0, 0);
-            lifes[i] = Instantiate(lifeImage, background[i].transform);
+            // Crea un objeto vacio
+            GameObject life = Instantiate(new GameObject(), parentLife.transform);
+            // Se le cambia el padre
+            life.transform.SetParent(parentLife.transform);
+            // Añade un componente Image
+            life.AddComponent<Image>().sprite = lifeImage;
+            // Cambia el Width y Height
+            life.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
+            // Cambia la posición añadiendo la separaciçon indicada en el inspector
+            life.transform.localPosition = new Vector3(i * separation, 0, 0);
+            lifes[i] = life;
         }
     }
 
     public void TakeDamage(int damage){
-        
+        currentLife -= damage;
+        if(currentLife <= 0){
+            currentLife = 0;
+            Debug.Log("Has morido");
+        }
+        UpdateLife();
+    }
+
+    public void AddLife(int life){
+        currentLife += life;
+        if(currentLife > maxLifes){
+            currentLife = maxLifes;
+            Debug.Log("Llevas demasiadas vidas");
+        }
+        UpdateLife();
+    }
+
+    private void UpdateLife(){
+        for (int i = 0; i < maxLifes; i++)
+        {
+            if (i < currentLife)
+            {
+                if(emptyLifeImage != null){
+                    lifes[i].GetComponent<Image>().sprite = lifeImage;
+                }else{
+                    lifes[i].SetActive(true);
+                }
+            }
+            else
+            {
+                if(emptyLifeImage != null){
+                    lifes[i].GetComponent<Image>().sprite = emptyLifeImage;
+                }else{
+                    lifes[i].SetActive(false);
+                }
+            }
+        }
     }
 }
