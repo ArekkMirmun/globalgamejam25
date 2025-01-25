49d7735cd0572f2a42dd5237157226dab1e2f8a8
VandurTheWizard <TheWizardVandur@gmail.com>
2025-01-25 19:21:11 +0100

    Realizado
    Ahora el jugador puede tener un sistema de partículas que actúa según la velocidad del jugador

---
+++ Assets/Scripts/Ivan/ParticleMovimg.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ParticleMovimg : MonoBehaviour
+{
+    [Header("Particle configuration")]
+    [SerializeField] private float minEmision = 1f;
+    [SerializeField] private float maxEmision = 1f;
+    [SerializeField] private ParticleSystem particleSystem;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(rb.linearVelocity.magnitude > 0)
+        {
+            var emission = particleSystem.emission;
+            emission.rateOverTime = maxEmision;
+        }
+        else
+        {
+            var emission = particleSystem.emission;
+            emission.rateOverTime = minEmision;
+        }
+    }
+}
