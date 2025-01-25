828d56e61d88cd978d63b05a13a0e0240e15f4a9
NuMeRo-999 <pedrovp003@gmail.com>
2025-01-25 12:28:19 +0100

    All enemies

---
+++ Assets/Scripts/Pedro/Enemies/EelTrigger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EelTrigger : MonoBehaviour
+{
+    public Eel parentEel;
+    public Transform movementPoint;
+
+    void Start()
+    {
+        // Obtener referencia al padre y al punto de movimiento
+        parentEel = GetComponentInParent<Eel>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && parentEel != null)
+        {
+            parentEel.StartMoving(movementPoint);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && parentEel != null)
+        {
+            parentEel.StopMoving();
+        }
+    }
+}
